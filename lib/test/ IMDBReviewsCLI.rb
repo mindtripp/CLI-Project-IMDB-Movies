9c76d742cfe2e58eb1c1bee3e9a1943f82c08b18
mindtripp <63605662+mindtripp@users.noreply.github.com>
2020-05-10 11:04:51 -0400

    Update and rename IMDB Reviews.rb to  IMDBReviewsCLI.rb

---
+++ lib/test/ IMDBReviewsCLI.rb
@@ -0,0 +1,22 @@
+class IMDBReviewsCLI::Display
+     def run
+        system("clear")
+        Display.greet
+        Display.get_name
+       Display.start_scraping 
+        print_all_reviews  
+        Display.prompt_to_pick_review
+    end
+    
+    def print_all_reviews
+      reviews = Review.all
+      reviews.each.with_index(1) do |review, index|
+            puts "== Review ##{index + 1} ==="
+            puts "movie rating:" + review.movie_rating
+            puts "movie title:" + review.movie_title
+            puts review.movie_summary
+            puts "=================="
+        end          
+    end
+end
+
