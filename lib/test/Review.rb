e2b6d2e679df57391d8c9159a8b111ddfb0218cc
mindtripp <63605662+mindtripp@users.noreply.github.com>
2020-05-10 11:05:40 -0400

    Update Review.rb

--- lib/test/Review.rb
+++ lib/test/Review.rb
@@ -1,28 +1,18 @@
 
 class Review
+  attr_accessor :movie_rating, :movie_title, :movie_summary, :link_to_summary 
 
   @@all = []
 
-  attr_accessor :movie_rating, :movie_title, :movie_summary, :link_to_summary
-  def initialize(movie_rating, movie_title, movie_summary) 
-    @movie_title = movie_title
+  def initialize(movie_rating, movie_title, link_to_summary) 
     @movie_rating = movie_rating
-    @movie_summary = movie_summary
+    @movie_title = movie_title
+    @link_to_summary = link_to_summary
     @@all << self
   end
 
   def self.all
     @@all
-  end
-
-  def self.print_all_reviews
-    self.all.each_with_index do |review, index|
-      puts "== Review ##{index + 1} ==="
-      puts "movie rating:" + review.movie_rating
-      puts "movie title:" + review.movie_title
-      puts review.movie_summary
-      puts "=================="
-    end
   end
 
 end
