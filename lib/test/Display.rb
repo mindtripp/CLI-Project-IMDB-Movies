b6c2d19fa85a5847df0d313c9428904a94a75e13
mindtripp <63605662+mindtripp@users.noreply.github.com>
2020-05-10 20:48:29 -0400

    Update Display.rb

--- lib/test/Display.rb
+++ lib/test/Display.rb
@@ -1,4 +1,4 @@
-class Display::IMDBReviewsCLI
+class Display
 
     def self.greet
         puts "Hello There Welcome to the IMDB Review!"
@@ -15,13 +15,25 @@ class Display::IMDBReviewsCLI
     def self.start_scraping
         puts "We're about to scrape..."
         puts "Please Wait a few moments..."
-        IMDBReviewScraper.scrape_and_make_reviews
+        puts IMDBReviewScraper.scrape_and_make_reviews
         puts "Movie reviews are ready!"
+    end
+
+    def self.print_all_reviews
+        #reviews = Review.all
+        #reviews.each.with_index(1) do |review, index|
+              puts "== Review  ==="  ###{index + 1}
+              puts "movie rating:" + Review.all.movie_rating
+              puts "movie title:" + Review.all.movie_title
+              puts review.movie_summary
+              puts "=================="
+          #end          
     end
     
     def self.prompt_to_pick_review
         puts "Which movie's summary you want to read? (Enter the number)"
     end
+
 
     def self.invalid_input
         puts "Invalid input, please try again (enter a number)."
