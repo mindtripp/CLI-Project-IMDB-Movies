05c2679622ca343a854fb53a71af8067a5a5b218
mindtripp <63605662+mindtripp@users.noreply.github.com>
2020-05-12 11:56:37 -0400

    Update IMDB ReviewScraper.rb

--- lib/test/IMDB ReviewScraper.rb
+++ lib/test/IMDB ReviewScraper.rb
@@ -3,21 +3,18 @@ class IMDBReviewScraper
 
   #first level scraping
   def self.scrape_and_make_reviews
-    homepage = Nokogiri::HTML.parse(open("https://www.imdb.com/?ref_=nv_home").read)
+    homepage = Nokogiri::HTML.parse(open("https://www.imdb.com/chart/top/?ref_=nv_mv_250")) 
     #binding.pry
-    homepage.css(".ipc-sub-grid-item.ipc-sub-grid-item--span-2").each do |element| #ipc-rating-star.ipc-rating-star--baseAlt.ipc-rating-star--imdb
-      #binding.pry
-      movie_rating = element.css("span.ipc-rating-star").text.to_i #ipc-rating-star ipc-rating-star--baseAlt ipc-rating-star--imdb
-      #Review.movie_rating = movie_rating
-      movie_title = element.css('a.ipc-poster-card__title').text  #ipc-poster-card__title ipc-poster-card__title--clamp-2 ipc-poster-card__title-href
-      #Review.movie_title = movie_title
-      link_to_summary = "https://www.imdb.com" + element.css('a.ipc-focusable').attribute("href").value
-      #Review.link_to_summary = link_to_summary
-      new_review = Review.new(movie_rating, movie_title, link_to_summary)
+
+    homepage.css('.lister-list').css('tr').each do |element|  
       
       #binding.pry
+      movie_rating = element.css(".ratingColumn.imdbRating").text #ipc-rating-star ipc-rating-star--baseAlt ipc-rating-star--imdb
+      movie_title = element.css('.titleColumn').text  #ipc-poster-card__title ipc-poster-card__title--clamp-2 ipc-poster-card__title-href
+      link_to_summary = "https://www.imdb.com" + element.css('a').attribute("href").value
+      new_review = Review.new(movie_rating, movie_title, link_to_summary)
+      #binding.pry
       scrape_review_blurb(new_review)
-
 
     end
     
@@ -31,6 +28,6 @@ class IMDBReviewScraper
           
         end
   end
-
 end
+
 
