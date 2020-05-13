5001ddf0e2bb782c217cb6481b72c4025dfaba15
mindtripp <63605662+mindtripp@users.noreply.github.com>
2020-05-13 15:30:35 -0400

    Update Review.rb

--- lib/test/Review.rb
+++ lib/test/Review.rb
@@ -9,10 +9,21 @@ class Review
     @movie_title = movie_title
     @link_to_summary = link_to_summary
     @@all << self
+    #puts "Making #{movie_rating}"
   end
 
   def self.all
     @@all
+  end
+
+  def self.print_all_reviews
+    Review.all.each_with_index do |review, index|
+          puts "== Review ##{index + 1} ===" 
+          puts "movie rating:" + review.movie_rating.split("\n")[1]
+          puts "movie title:" + review.movie_title.split("\n")[2]
+          puts review.movie_summary
+          puts "==================" 
+    end          
   end
 
 end
