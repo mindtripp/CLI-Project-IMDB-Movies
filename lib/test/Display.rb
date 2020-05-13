8180436f4ea8605c3705917a8c32cd1d3296dc02
mindtripp <63605662+mindtripp@users.noreply.github.com>
2020-05-13 15:29:31 -0400

    Update Display.rb

--- lib/test/Display.rb
+++ lib/test/Display.rb
@@ -1,7 +1,7 @@
 class Display
 
     def self.greet
-        puts "Hello There Welcome to the IMDB Review!"
+        puts "Hello there, welcome to the IMDB Movie Review!"
         puts ""
         puts ""
         puts "What is your name?"
@@ -9,6 +9,7 @@ class Display
     
     def self.get_name
         name = gets.chomp
+        puts ""
         puts "Welcome, #{name}"
     end
     
@@ -16,22 +17,35 @@ class Display
         puts "We're about to scrape..."
         puts "Please Wait a few moments..."
         IMDBReviewScraper.scrape_and_make_reviews
-        puts "Movie reviews are ready!"
+        puts "Movie list is ready!"
     end
 
     
     def self.prompt_to_pick_review
-        puts "Which movie's summary you want to read? (Enter the number)"
+        puts "Suprise! We actually have 250 movie in our inventory." 
+        puts "Pick your lucky number to see the corresponding movie summary: (Enter a number from 1 - 250)"
     end
 
 
-    def self.invalid_input
-        puts "Invalid input, please try again (enter a number)."
-    end
+    # def self.validation_input(input)
+    #     if input.to_i <= Review.all.size
+    #         validation = true
+    #         review = Review.all[input.to_i-1]
+    #         puts "Here is your selected movie's summary:"
+    #         puts review.movie_title.split("\n")[2]
+    #         puts review.movie_summary
+    #     elsif input.to_i > Review.all.size
+    #         validation = false
+    #         puts "Please choose a number from 1-250."
+    #     else
+    #         validation = false
+    #         puts "Invalid input, please try again (Enter a number)."
+    #     end
+    # end
 
-    def self.print_selected_summary(movie_summary)
-        puts "Here is your selected movie's summary: + #{selected_summary.movie_summary}"
-    end
+    # def self.print_selected_summary(movie_summary)
+    #     puts "Here is your selected movie's summary: + #{selected_summary.movie_summary}"
+    # end
 
 end
 
