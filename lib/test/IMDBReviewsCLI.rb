174953f0f3890941de80b8950ceb13099fa96966
mindtripp <63605662+mindtripp@users.noreply.github.com>
2020-05-13 15:30:01 -0400

    Update IMDBReviewsCLI.rb

--- lib/test/IMDBReviewsCLI.rb
+++ lib/test/IMDBReviewsCLI.rb
@@ -1,27 +1,48 @@
 class IMDBReviewsCLI
-     def run
+
+    def run
         system("clear")
         Display.greet
         Display.get_name
         Display.start_scraping 
-        Review.print_all_reviews  
-        Display.prompt_to_pick_review
-        input = gets.strip
-        input.to_i <= Review.all.size
-        review = Review.all[input.to_i-1]
-        puts review.movie_summary
+        Review.print_all_reviews
 
+        validation = false
+        while validation == false do
+            Display.prompt_to_pick_review
+            input = gets.strip
+                if 0 < input.to_i and input.to_i <= Review.all.size
+                    validation == true
+                    review = Review.all[input.to_i-1]
+                    puts "Here is your selected movie's summary:"
+                    puts review.movie_title.split("\n")[2]
+                    puts review.movie_summary
+                    puts ""
+                    puts "Thanks for visiting IMDB Reviews!"
+                    break
+                elsif input.to_i > Review.all.size
+                    validation == false
+                    puts "Please choose a number from 1-250."
+                elsif input.to_i == 0
+                    validation == false
+                    puts "Invalid input, please try again (Enter a number)."
+                end
+        end
+        # validation = false
+        # until validation
+        #     Display.prompt_to_pick_review
+        #     input = gets.strip
+
+        #         if input.to_i <= Review.all.size
+        #             review = Review.all[input.to_i-1]
+        #             puts "Here is your selected movie's summary:"
+        #             puts review.movie_title.split("\n")[2]
+        #             puts review.movie_summary
+        #             validation = true
+        #         end
+                
+        # end
     end
-    
-    
-    #  def print_all_reviews
-    #    reviews = Review.all
-    #   reviews.each.with_index(1) do |review, index|
-    #         puts "== Review ##{index + 1} ==="
-    #         puts "movie rating:" + review.movie_rating
-    #         puts "movie title:" + review.movie_title
-    #         puts review.movie_summary
-    #         puts "=================="
-    #     end          
-    # end
+
 end
+
